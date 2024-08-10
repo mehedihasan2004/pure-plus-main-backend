1b364efb87144cbf865e23dd787baac2284b2cbb
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-10 20:16:52 +0600

    refactor: replace all interface to type

--- src/errors/handle-prisma-client-error.ts
+++ src/errors/handle-prisma-client-error.ts
@@ -1,8 +1,5 @@
 import { Prisma } from '@prisma/client';
-import {
-  GenericErrorMessage,
-  GenericErrorResponse,
-} from '../interfaces/common';
+import { GenericErrorMessage, GenericErrorResponse } from '../types/common';
 
 const handlePrismaClientError = (
   error: Prisma.PrismaClientKnownRequestError,
