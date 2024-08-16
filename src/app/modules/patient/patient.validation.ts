bef72811653a8c1bdc4a8d3c01b863fb0b0ff1a7
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-16 15:39:26 +0600

    feat: update create an user with patient zod schema

--- src/app/modules/patient/patient.validation.ts
+++ src/app/modules/patient/patient.validation.ts
@@ -1,5 +1,23 @@
 import { z } from 'zod';
+import { UserValidation } from '../user/user.validation';
 
-const createAnUserWithPatientZodSchema = z.object({});
+const createAPatientZodSchema = z.object({
+  address: z.string().optional(),
+  emergencyContactNumber: z
+    .string()
+    .min(11, {
+      message: 'Energency contact number should be at lest 11 character!',
+    })
+    .optional(),
+
+  userId: z.string({ required_error: 'User id is required!' }),
+});
+
+const createAnUserWithPatientZodSchema = z.object({
+  body: z.object({
+    user: UserValidation.createAnUserZodSchema,
+    patient: createAPatientZodSchema,
+  }),
+});
 
 export const PatientValidation = { createAnUserWithPatientZodSchema };
