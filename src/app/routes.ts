387f3d3fbb1fa0a5e10bf9ed176dba1d3706ce6b
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-16 15:47:12 +0600

    feat: add the patient route in global route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { UserRoutes } from './modules/user/user.route';
 import { DoctorRoutes } from './modules/doctor/doctor.route';
+import { PatientRoutes } from './modules/patient/patient.route';
 import { AppointmentRoutes } from './modules/appointment/appointment.route';
 
 const router = Router();
@@ -8,6 +9,7 @@ const router = Router();
 [
   { path: '/users', route: UserRoutes },
   { path: '/doctors', route: DoctorRoutes },
+  { path: '/patients', route: PatientRoutes },
   { path: '/appointments', route: AppointmentRoutes },
 ].forEach(({ path, route }) => router.use(path, route));
 
